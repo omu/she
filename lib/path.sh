e7e5a4e285e260096fe0ec84e0f8599726e0c22a
Recai Oktaş <roktas@gmail.com>
2019-09-24 18:16:05 +0300

    .

--- lib/path.sh
+++ lib/path.sh
@@ -16,3 +16,52 @@ path.is_inside() {
 
 	[[ ! $relative =~ ^[.] ]]
 }
+
+path.dir() {
+	local -n path_dir_=${1?missing 1th argument: name reference}
+
+	case $path_dir_ in
+	*/*)
+		path_dir_=${path_dir_%/*}
+		[[ -n $path_dir_ ]] || path_dir_=/
+		;;
+	*)
+		path_dir_=.
+		;;
+	esac
+}
+
+path.base() {
+	local -n path_base_=${1?missing 1th argument: name reference}
+
+	path_base_=${path_base_##*/}
+}
+
+path.ext() {
+	local -n path_ext_=${1?missing 1th argument: name reference}
+
+	path_ext_=${path_ext_##*/}
+
+	case $path_ext_ in
+	*.*)
+		path_ext_=${path_ext_##*.}
+		;;
+	*)
+		path_ext_=
+		;;
+	esac
+}
+
+path.ext_change() {
+	local -n path_ext_change_=${1?missing 1th argument: name reference}
+	local ext=${2?missing 2nd argument: ext}
+
+	case $path_ext_change_ in
+	*.*)
+		path_ext_change_=${path_ext_change_%.*}
+		path_ext_change_=${path_ext_change_}${ext}
+		;;
+	*)
+		;;
+	esac
+}
