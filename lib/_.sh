9b0095bd19f2f2425ef72388c8949623da93ceae
Recai Oktaş <roktas@gmail.com>
2019-10-30 09:44:15 +0300

    .

--- lib/_.sh
+++ lib/_.sh
@@ -213,6 +213,15 @@ _.should() {
 	"$@"
 }
 
+.stacktrace() {
+	local -i i=1
+
+	while [[ -n ${BASH_SOURCE[$i]:-} ]]; do
+		echo "${BASH_SOURCE[$i]}":"${BASH_LINENO[$((i-1))]}":"${FUNCNAME[$i]}"\(\)
+		i=$((i + 1))
+	done | grep -v "^${BASH_SOURCE[0]}"
+}
+
 # Initialize underscore system
 
 .init() {
