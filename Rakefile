b45c78c5bde23e0029559af51b723fe2babcdbf6
Recai Oktaş <roktas@gmail.com>
2019-12-14 23:53:26 +0300

    .

--- Rakefile
+++ Rakefile
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-BIN = %w[_ t tap].freeze
+BIN = %w[_ t x tap].freeze
 
 desc 'Build'
 task :build do
---
+++ cmd/x/main.sh
@@ -0,0 +1,25 @@
+#=github.com/omu/home/src/sh/!.sh
+
+#=github.com/omu/home/src/sh/color.sh
+#=github.com/omu/home/src/sh/callback.sh
+#=github.com/omu/home/src/sh/defer.sh
+#=github.com/omu/home/src/sh/file.sh
+#=github.com/omu/home/src/sh/filetype.sh
+#=github.com/omu/home/src/sh/flag.sh
+#=github.com/omu/home/src/sh/git.sh
+#=github.com/omu/home/src/sh/http.sh
+#=github.com/omu/home/src/sh/ui.sh
+#=github.com/omu/home/src/sh/temp.sh
+#=github.com/omu/home/src/sh/url.sh
+#=github.com/omu/home/src/sh/zip.sh
+#=github.com/omu/home/src/sh/src.sh
+
+#:cmd/x/x.sh
+
+if [[ "${BASH_SOURCE[0]}" = "$0" ]]; then
+	main() {
+		:
+	}
+
+	main "$@"
+fi
